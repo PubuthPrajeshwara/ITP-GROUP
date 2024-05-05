2ed74ed8ca1d0a74e07f423aebccd9af2acf5d58
Dilshan Pathum <pathumpanagoda@gmail.com>
2024-05-05 15:21:48 +0530

    add service form

--- admin/src/components/serviceComp/ServiceForm/ServiceForm.jsx
+++ admin/src/components/serviceComp/ServiceForm/ServiceForm.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import './ServiceForm.css';
 import axios from 'axios'; // Import axios for making HTTP requests
+import upload_img from '../../../assets/upload_img.png';
+
 
 const AddService = () => {
+  const [image,setImage] = useState(false);
     const [service, setService] = useState({
       serviceTitle: '',
       estimatedHour: '',
         details: '',
     }); 
+
+    const imageHandler = (e) =>{
+      setImage(e.target.files[0]);
+  }
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -37,10 +44,13 @@ const AddService = () => {
             <div className="f-container">
                 <form onSubmit={handleSubmit}>
                   <div className='img-container'>
-                    <img src="car-service.jpg" alt="Car Service" className=""/>
+                  <div className='addproduct-item'>
+              <label htmlFor="file-input">
+                <img src={image?URL.createObjectURL(image):upload_img} className='addproduct-thumbnail' alt="Upload Thumbnail" />
+              </label>
+              <input onChange={imageHandler} type='file' name='image' id='file-input' hidden={true} />
+            </div>  
                     </div>
-                    <button className="import-button">Import</button>
-
                     <label className='lab'>Service Title:</label>
                     <input 
                         type="text" 
